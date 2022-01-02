bac0908dbf573f1b3ae10ff89be4fa3870d9daa2
alvinlouis <71868909+alvinlouis@users.noreply.github.com>
2022-01-02 19:08:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -168,207 +168,86 @@ cakeTop.receiveShadow = true;
 scene.add(cakeTop);
 
 //decor codes
-//chocolate ball
-const choco = new THREE.Mesh(
-    new THREE.SphereGeometry(0.3,32,32),
-    new THREE.MeshPhongMaterial({                
-        color: 0x5e350C
-      })
-);
-choco.position.set(-20,-5,10);
-choco.castShadow = true;
-choco.receiveShadow = true;
 
-scene.add(choco);
+//cream & choco
+const chocoBottom = [];
+const chocoTop = [];
+var generateChoco = () => {
+    let chocoGeo = new THREE.SphereGeometry(0.3,32,32)
+    let chocoMat = new THREE.MeshPhongMaterial({color: 0x5e350C})
+    let chocoMesh = new THREE.Mesh(chocoGeo,chocoMat)
+    chocoMesh.position.set(-20,-5,10);
+    chocoMesh.castShadow = true;
+    chocoMesh.receiveShadow = true;
+    scene.add(chocoMesh)
+    return chocoMesh
+}
 
-//cream (1-10 for bottom cake, 11-20 for top cake)
+const creamBottom = [];
+const creamTop = [];
+var generateCream = () => {
+    let creamGeo = new THREE.TorusGeometry(0.3,0.3,30,64)
+    let creamMat = new THREE.MeshPhongMaterial({})
+    let creamMesh = new THREE.Mesh(creamGeo,creamMat)
+    creamMesh.rotation.x = Math.PI/2;
+    creamMesh.castShadow = true;
+    creamMesh.receiveShadow = true;
+    scene.add(creamMesh)
+    return creamMesh
+}
 
-///////////////////////////////////////////////////
-//cream 1
-const cream1 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
 //𝑥 = (𝑐𝑎𝑘𝑒 𝑟𝑎𝑑𝑖𝑢𝑠 – decoration size * 2 – 0.1) * sin (n-decoration * 360 / total decoration)
+//y = top of bottom cake  
 //𝑧 = (𝑐𝑎𝑘𝑒 𝑟𝑎𝑑𝑖𝑢𝑠 – decoration size * 2 – 0.1) * cos (n-decoration * 360 / total decoration)    
-//x = (5 - 0.3 * 2 - 0.1) * sin (0 * 360 / 10) + (- 20) (ps. minus 20 because of initial position)      
-//y = top of bottom cake    
-//z = (5 - 0.3 * 2 - 0.1) * sin (0 * 360 / 10) + 10     (ps. add 10 because of initial position)
-// cream1.position.set(-24.2,-5,10-0.05);
-cream1.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((1 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((1 * 360) / 10)) + 10
-    );
-cream1.rotation.x = Math.PI/2; //𝜋/ 2 𝑟𝑎𝑑 = 90 degree //idk if it's x or y axis based on the 2nd rotation, but following the reference picture it's using x axis
-cream1.castShadow = true;
-cream1.receiveShadow = true;
+for(let i = 0; i < 10; i++){
+    creamBottom[i] = generateCream();
+    creamBottom[i].rotation.x = Math.PI/2;
+    creamBottom[i].position.set( 
+                            ((5 - (0.3 * 2)) - 0.1) * (Math.sin((i * 360) / 10)) - 20,
+                            -5,
+                            ((5 - (0.3 * 2)) - 0.1) * (Math.cos((i * 360) / 10)) + 10);
+    
+    chocoBottom[i] = generateChoco();                            
+    chocoBottom[i].position.set( 
+                                ((5 - (0.3 * 2)) - 0.1) * (Math.sin((i * 360) / 10)) - 20,
+                                -4.5,
+                                ((5 - (0.3 * 2)) - 0.1) * (Math.cos((i * 360) / 10)) + 10);                            
+}
 
-scene.add(cream1);
+for(let i = 0; i < 10; i++){
+    creamTop[i] = generateCream(); 
+    creamTop[i].rotation.x = Math.PI/2;
+    creamTop[i].position.set( 
+                            ((3 - (0.3 * 2)) - 0.1) * (Math.sin((i * 360) / 10)) - 20,
+                            -1,
+                            ((3 - (0.3 * 2)) - 0.1) * (Math.cos((i * 360) / 10)) + 10);
 
+    chocoTop[i] = generateChoco();                            
+    chocoTop[i].position.set( 
+                            ((3 - (0.3 * 2)) - 0.1) * (Math.sin((i * 360) / 10)) - 20,
+                            -0.5,
+                            ((3 - (0.3 * 2)) - 0.1) * (Math.cos((i * 360) / 10)) + 10);                        
+}
 
-//cream 2
-const cream2 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream2.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((2 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((2 * 360) / 10)) + 10
-    );
-cream2.rotation.x = Math.PI/2;
-cream2.castShadow = true;
-cream2.receiveShadow = true;
+//text loader following this tutorial : https://www.youtube.com/watch?v=IA3HjAV2nzU
 
-scene.add(cream2);
+var loader = new FontLoader();
+loader.load('./three.js-master/examples/fonts/helvetiker_bold.typeface.json',function(font){
+    var textGeo = new TextGeometry('Happy Birthday',{
+       font: font,
+       size: 0.4,
+       height: 0.05,
+    }); 
+    var textMesh = new THREE.Mesh(textGeo, 
+                                new THREE.MeshStandardMaterial({color: 0xD65645})
+                                  );
+    textMesh.castShadow = true;
+    textMesh.receiveShadow = true;
+    textMesh.position.set(-20,0,12);   
+    textMesh.rotation.set(- (Math.PI/8), 90*(Math.PI/180), 0);
 
-//cream 3
-const cream3 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream3.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((3 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((3 * 360) / 10)) + 10
-    );
-cream3.rotation.x = Math.PI/2;
-cream3.castShadow = true;
-cream3.receiveShadow = true;
-
-scene.add(cream3);
-
-//cream 4
-const cream4 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream4.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((4 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((4 * 360) / 10)) + 10
-    );
-cream4.rotation.x = Math.PI/2;
-cream4.castShadow = true;
-cream4.receiveShadow = true;
-
-scene.add(cream4);
-
-//cream 5
-const cream5 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream5.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((5 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((5 * 360) / 10)) + 10
-    );
-cream5.rotation.x = Math.PI/2;
-cream5.castShadow = true;
-cream5.receiveShadow = true;
-
-scene.add(cream5);
-
-//cream 6
-const cream6 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream6.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((6 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((6 * 360) / 10)) + 10
-    );
-cream6.rotation.x = Math.PI/2;
-cream6.castShadow = true;
-cream6.receiveShadow = true;
-
-scene.add(cream6);
-
-//cream 7
-const cream7 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream7.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((7 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((7 * 360) / 10)) + 10
-    );
-cream7.rotation.x = Math.PI/2;
-cream7.castShadow = true;
-cream7.receiveShadow = true;
-
-scene.add(cream7);
-
-//cream 8
-const cream8 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream8.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((8 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((8 * 360) / 10)) + 10
-    );
-cream8.rotation.x = Math.PI/2;
-cream8.castShadow = true;
-cream8.receiveShadow = true;
-
-scene.add(cream8);
-
-//cream 9
-const cream9 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream9.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((9 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((9 * 360) / 10)) + 10
-    );
-cream9.rotation.x = Math.PI/2;
-cream9.castShadow = true;
-cream9.receiveShadow = true;
-
-scene.add(cream9);
-
-//cream 10
-const cream10 = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3,0.3,30,64),
-    new THREE.MeshPhongMaterial({})
-);
-cream10.position.set(    
-    ((5 - (0.3 * 2)) - 0.1) * (Math.sin((10 * 360) / 10)) - 20,        
-    -5,
-    ((5 - (0.3 * 2)) - 0.1) * (Math.cos((10 * 360) / 10)) + 10
-    );
-cream10.rotation.x = Math.PI/2;
-cream10.castShadow = true;
-cream10.receiveShadow = true;
-
-scene.add(cream10);
-
-//text
-const fontLoader = new FontLoader();
-fontLoader.load('./three.js-master/examples/fonts/helvetiker_bold.typeface.json', function ( font ){
-    const text = new THREE.Mesh(
-    new THREE.TextGeometry('Happy Birthday',{font: font,size: 40}),
-    new THREE.MeshStandardMaterial({color: 0xD65645})
-    );
-    text.position.set(20,4,10);
-    text.castShadow = true;
-    text.receiveShadow = true;
-    text.rotation.set(-Math.PI / 8, Math.PI / 2, 0); //Euler (− 𝜋/ 8 𝑟𝑎𝑑, 𝜋/ 2 𝑟𝑎𝑑, 0)
-
-    scene.add(text);
+    scene.add(textMesh);
 });
-
-
-//decor codes
-
 
 //to render the whole code :v
 function animate(){
